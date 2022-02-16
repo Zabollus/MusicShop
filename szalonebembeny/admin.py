df3172d693d58737e3b858078c186c0bb1edd5e8
Tomasz Jakubiak <tomek.jakubiak94@gmail.com>
2022-02-16 19:20:55 +0100

    dodanie modeli do admina

--- szalonebembeny/admin.py
+++ szalonebembeny/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from szalonebembeny.models import Product, Category, Profile, Comment
 # Register your models here.
+
+
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Profile)
+admin.site.register(Comment)
