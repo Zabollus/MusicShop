7c2f1f84fe2157906f4d26ebc82ed3f78e274778
Tomasz Jakubiak <tomek.jakubiak94@gmail.com>
2022-02-18 00:18:25 +0100

    główne szablony

--- MusicShop/urls.py
+++ MusicShop/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from szalonebembeny.views import LandingPageView, ProductsView, CategoriesView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', LandingPageView.as_view(), name='main'),
+    path('products/', ProductsView.as_view(), name='products'),
+    path('categories/', CategoriesView.as_view(), name='categories')
 ]
--- szalonebembeny/views.py
+++ szalonebembeny/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views import View
+from django.shortcuts import render
+from szalonebembeny.models import Product, Category
 
 # Create your views here.
+
+
+class LandingPageView(View):
+    def get(self, request):
+        return render(request, 'index.html')
+
+
+class ProductsView(View):
+    def get(self, request):
+        products = Product.objects.all()
+        return render(request, 'products.html', {'products': products})
+
+
+class CategoriesView(View):
+    def get(self, request):
+        categories = Category.objects.all()
+        return render(request, 'categories.html', {'categories': categories})
